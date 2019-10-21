c9263528867f4fd75a080228203a25b9300c5c9d
Eugene Rush <erush91@gmail.com>
2019-10-21 13:00:01 -0600

    first commit

---
+++ src/tf2odom.cpp
@@ -0,0 +1,57 @@
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/TransformStamped.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "tf2odom_publisher");
+
+  ros::NodeHandle node;
+
+  ros::Publisher tf_odom_pub = 
+    node.advertise<nav_msgs::Odometry>("tf_odom", 10);
+
+  ros::Publisher tf_point_pub = 
+    node.advertise<geometry_msgs::PointStamped>("tf_point", 1);
+  
+  tf::TransformListener listener;
+  nav_msgs::Odometry odom_msg;
+  geometry_msgs::TransformStamped tf_msg;
+  geometry_msgs::Quaternion quat_msg;
+
+  geometry_msgs::PointStamped point_msg; 
+
+  odom_msg.header.frame_id = "odom"; 
+  ros::Rate rate(200);
+  while (node.ok()){
+    tf::StampedTransform transform;
+    try{
+      listener.lookupTransform("/odom", "/base_link",  
+                               ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(0.1).sleep();
+    }
+    odom_msg.header.stamp = ros::Time::now();
+    odom_msg.pose.pose.position.x = transform.getOrigin().x();
+    odom_msg.pose.pose.position.y = transform.getOrigin().y();
+    odom_msg.pose.pose.position.z = transform.getOrigin().z();
+    tf::quaternionTFToMsg(transform.getRotation().normalize(), odom_msg.pose.pose.orientation);
+
+    tf_odom_pub.publish(odom_msg);
+
+    point_msg.header.stamp = ros::Time::now();
+    point_msg.point.x = odom_msg.pose.pose.position.x;
+    point_msg.point.y = odom_msg.pose.pose.position.y;
+    point_msg.point.z = odom_msg.pose.pose.position.z;
+
+    tf_point_pub.publish(point_msg);
+
+    rate.sleep();
+  }
+  return 0;
+};
